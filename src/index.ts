67152e06230835499a2e4bade9d448a8e5434e44
mitrofanzxc <mitrofanzxc@gmail.com>
2022-11-15 16:11:49 +0300

    feat: add view for form

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
 import './index.scss';
+
+const buttonSubmit = document.querySelector('.form__submit') as HTMLInputElement;
+
+function submit(event: Event) {
+  event.preventDefault();
+}
+
+buttonSubmit.addEventListener('click', submit);
