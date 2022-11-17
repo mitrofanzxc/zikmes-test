398f2c8e344800018f2c5e34761066b961ad1d66
mitrofanzxc <mitrofanzxc@gmail.com>
2022-11-17 12:44:16 +0300

    refactor: rewrite toast logic on class

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
+import Toast from './utils/Toast';
 import './index.scss';
 
 const FORM = document.querySelector('.form') as HTMLFormElement;
 const INPUT_TEL = FORM.querySelector('.form__input') as HTMLInputElement;
 const BUTTON_SUBMIT = FORM.querySelector('.form__submit') as HTMLButtonElement;
-const MODAL = FORM.querySelector('.modal') as HTMLDivElement;
-const MODAL_MESSAGE = MODAL.querySelector('.modal__message') as HTMLSpanElement;
 
 const regexBY = /^\+?375(\s+)?\(?(17|29|33|44)\)?(\s+)?[0-9]{3}-?[0-9]{2}-?[0-9]{2}$/;
 const regexRU = /^((8|\+?7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/;
@@ -13,31 +12,16 @@ function submit(event: Event) {
   event.preventDefault();
 
   if (!INPUT_TEL.value) {
-    createModal('Введите свой номер телефона!');
+    new Toast('Введите свой номер телефона!').create();
   } else if (
     (!regexBY.test(INPUT_TEL.value) || !regexRU.test(INPUT_TEL.value)) &&
     INPUT_TEL.value
   ) {
-    createModal('Введите корректный номер телефона!');
+    new Toast('Введите корректный номер телефона!').create();
   } else {
-    createModal('Ваш заказ находится в обработке, совсем скоро мы с вами свяжемся!');
+    new Toast('Ваш заказ находится в обработке, совсем скоро мы с вами свяжемся!').create();
   }
-}
-
-function createModal(value: string) {
-  MODAL_MESSAGE.textContent = ` ${value}`;
-  MODAL.classList.add('modal_animation-create');
-  setTimeout(() => MODAL.classList.remove('modal_animation-create'), 1000);
-  setTimeout(() => MODAL.classList.add('modal_animation-remove'), 1000);
 }
 
 FORM.addEventListener('submit', submit);
 BUTTON_SUBMIT.addEventListener('click', submit);
-
-MODAL.addEventListener('animationstart', () => {
-  BUTTON_SUBMIT.setAttribute('disabled', '');
-});
-
-MODAL.addEventListener('animationend', () => {
-  BUTTON_SUBMIT.removeAttribute('disabled');
-});
